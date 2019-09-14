95b9276255d5de55364f259ccff317cf4fc6f7a3
unknown <1danabana@gmail.com>
2019-09-13 17:25:32 -0700

    adding all the npm installs and the basic html servey questions

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+var friend = [
+    {
+        name: "lance thunderchad0",
+        photo: "Image.png",
+        scores: [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+    },
+    {
+        name: "lance thunderchad1",
+        photo: "Image.png",
+        scores: [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+    },
+    {
+        name: "lance thunderchad2",
+        photo: "Image.png",
+        scores: [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+    },
+    {
+        name: "lance thunderchad3",
+        photo: "Image.png",
+        scores: [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+    },
+    {
+        name: "lance thunderchad4",
+        photo: "Image.png",
+        scores: [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+    },
+    {
+        name: "lance thunderchad5",
+        photo: "Image.png",
+        scores: [5, 4, 3, 2, 1, 6, 5, 4, 3, 2]
+    },
+];
